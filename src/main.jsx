480fe699c8f871ced48da9e9519f5a5aedea216a
Rayyan0080 <rayyanomarz@gmail.com>
2026-06-13 13:47:18 -0400

    aa

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// 1. Import HashRouter instead of BrowserRouter
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* 2. HashRouter doesn't need a tricky basename! */}
     <HashRouter>
       <App />
     </HashRouter>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // This tells GitHub pages exactly where your files are
-  base: "/memory_game_/",
+  // The dot makes it work ANYWHERE, ignoring repo names completely
+  base: "./",
   plugins: [react()],
 });
