87958ba1ddb9329ec9ac37c89d73f1d4025275f7
Rayyan0080 <rayyanomarz@gmail.com>
2026-06-13 13:24:34 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  // This tells GitHub pages exactly where your files are
+  base: "/memory_game_/",
   plugins: [react()],
-})
+});
